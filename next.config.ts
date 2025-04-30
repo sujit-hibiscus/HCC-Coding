7deb089fb570780f00253873abe81bca32a8d593
sujit1138 <sujitbhanderi331@gmail.com>
2025-04-30 11:16:19 +0530

    fix: Next config file

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   poweredByHeader: false,
-  async redirects() {
-    return [];
-  },
-  trailingSlash: false, // Optional, helps with route consistency
+  trailingSlash: false,
 };
 
 export default nextConfig;
