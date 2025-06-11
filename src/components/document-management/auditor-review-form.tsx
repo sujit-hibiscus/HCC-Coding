0b7337ea4017f5a7fd87ba34ad9222758c81c026
sujit1138 <sujitbhanderi331@gmail.com>
2025-06-11 11:19:06 +0530

    Enable: Developer tool protection

--- src/components/document-management/auditor-review-form.tsx
+++ src/components/document-management/auditor-review-form.tsx
@@ -530,7 +530,7 @@ export default function AuditorReviewForm({
 
                     {/* Optimized Code Cards */}
                     <div className="flex-1 min-h-0">
-                        <div className="h-full space-y-2 pr-1 overflow-y-auto overflow-x-hidden max-h-[520px]">
+                        <div className="h-full space-y-2 pr-1 overflow-y-auto overflow-x-hidden max-h-[540px]">
                             {isCodeLoading ? (
                                 <motion.div
                                     className="flex flex-col items-center justify-center py-12 text-center"
